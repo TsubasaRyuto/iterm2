50ac45feb99f2dd5f65464b4de8b6127d60d7aa2
TsubasaRyuto <tsubasa7ryuto@gmail.com>
2024-03-02 21:09:35 +0900

    setup sh

---
+++ neovim/init.lua
@@ -0,0 +1,65 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+  vim.api.nvim_command("packadd packer.nvim")
+end
+
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'dracula/vim'
+  use 'hoob3rt/lualine.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use 'tpope/vim-fugitive'
+end)
+
+-- 基本設定
+vim.o.backspace = "indent,eol,start"
+
+-- Emmet-vimの設定
+vim.g.user_emmet_leader_key = "<c-f>"
+
+-- その他の主要な設定
+vim.o.encoding = "utf-8"
+vim.o.scriptencoding = "utf-8"
+vim.o.nobackup = true
+vim.o.noswapfile = true
+vim.o.autoread = true
+vim.o.clipboard = "unnamed"
+vim.o.belloff = "all"
+vim.o.mouse = "a"
+
+-- 外観の設定
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.smartindent = true
+vim.o.showmatch = true
+vim.o.listchars = "tab:^\\ ,trail:~"
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.title = true
+vim.o.display = "lastline"
+vim.o.list = true
+vim.o.completeopt = "menuone"
+
+-- 検索設定
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.smartcase = true
+vim.o.wrapscan = true
+
+-- 矢印キーを無効にする
+vim.api.nvim_set_keymap("n", "<Up>", "<Nop>", {})
+vim.api.nvim_set_keymap("n", "<Down>", "<Nop>", {})
+vim.api.nvim_set_keymap("n", "<Left>", "<Nop>", {})
+vim.api.nvim_set_keymap("n", "<Right>", "<Nop>", {})
+vim.api.nvim_set_keymap("i", "<Up>", "<Nop>", {})
+vim.api.nvim_set_keymap("i", "<Down>", "<Nop>", {})
+vim.api.nvim_set_keymap("i", "<Left>", "<Nop>", {})
+vim.api.nvim_set_keymap("i", "<Right>", "<Nop>", {})
+
+vim.cmd 'colorscheme dracula'
---
+++ setup.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Homebrewがインストールされているか確認
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrewがインストールされていません。Homebrewをインストールします"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    exit
+fi
+
+# Git, Node.jsのインストール
+if ! command -v git &> /dev/null; then
+    echo "Gitをインストールしています..."
+    brew install git
+fi
+
+if ! command -v node &> /dev/null; then
+    echo "Node.jsをインストールしています..."
+    brew install node
+fi
+
+# Oh My Zshのインストール
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zshをインストールしています..."
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# zsh-autosuggestionsのインストール
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    echo "zsh-autosuggestionsをインストールしています..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+# zsh-syntax-highlightingのインストール
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    echo "zsh-syntax-highlightingをインストールしています..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+cp ./zsh/.zshrc ~/.zshrc
+
+# LSPサーバーのインストール (例: pyright, typescript-language-server)
+echo "LSPサーバーをインストールしています..."
+npm install -g pyright typescript-language-server
+
+# tmuxのインストール
+echo "tmuxをインストールしています..."
+brew install tmux
+
+# Neovimのインストール
+echo "Neovimをインストールしています..."
+brew install neovim
+
+# Packerのインストール
+if [ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/packer/start/packer.nvim ]; then
+    echo "Packerをインストールしています..."
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/packer/start/packer.nvim
+fi
+
+# .tmux.confをコピー
+cp ./tmux/.tmux.conf ~/.tmux.conf
+
+# 設定ファイルのコピー
+NVIM_CONFIG_DIR="$HOME/.config/nvim"
+
+if [ ! -d "$NVIM_CONFIG_DIR" ]; then
+    mkdir -p "$NVIM_CONFIG_DIR"
+fi
+
+# init.luaと他の設定ファイルをコピー
+echo "設定ファイルをコピーしています..."
+cp ./neovim/init.lua "$NVIM_CONFIG_DIR"
+cp ./neovim/lazyvim.json "$NVIM_CONFIG_DIR"
+cp ./neovim/lazy-lock.json "$NVIM_CONFIG_DIR"
+
+# ディレクトリ構成をコピー
+echo "ディレクトリ構成をコピーしています..."
+cp -r ./neovim/pack "$NVIM_CONFIG_DIR"
+cp -r ./neovim/starter "$NVIM_CONFIG_DIR"
+cp -r ./nowvim/lua "$NVIM_CONFIG_DIR"
+cp -r ./neovim/plugin "$NVIM_CONFIG_DIR"
+
+# Neovimを起動し、Packerを使ってプラグインをインストールするコマンドを実行
+nvim --headless +PackerInstall +qall
+
+# Gitの設定ファイルをコピー
+cp ./git/.gitconfig ~/.gitconfig
+
+echo "セットアップが完了しました。"
